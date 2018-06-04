dab74b5196e96a7428663e2cdf0773d4f74080fd
Paul Evans <decretist@gmail.com>
2018-06-04 10:42:28 -0700

    Split Sg case statements
    Split sg case statements by hand into three samples (a, b, and c)
    of nearly equal word count.

---
+++ samples/sg_case_statements/rollup3.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cat "C.P d.init.txt" "C.1 d.init.txt" "C.2 d.init.txt" "C.3 d.init.txt" "C.4 d.init.txt" "C.5 d.init.txt" "C.6 d.init.txt" "C.7 d.init.txt" "C.8 d.init.txt" >> Gratian0a.txt
+cat "C.9 d.init.txt" "C.10 d.init.txt" "C.11 d.init.txt" "C.12 d.init.txt" "C.13 d.init.txt" "C.14 d.init.txt" "C.15 d.init.txt" "C.16 d.init.txt" "C.17 d.init.txt" "C.18 d.init.txt" "C.19 d.init.txt" "C.20 d.init.txt" >> Gratian0b.txt
+cat "C.21 d.init.txt" "C.22 d.init.txt" "C.23 d.init.txt" "C.27 d.init.txt" "C.29 d.init.txt" "C.30 d.init.txt" "C.31 d.init.txt" "C.32 d.init.txt" "C.33 d.init.txt" "C.34 d.init.txt" "C.35 d.init.txt" "C.36 d.init.txt" >> Gratian0c.txt
